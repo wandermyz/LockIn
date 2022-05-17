eb2e9ae11dda79c31aed3f2348fdad8fd06c9ab9
Yang Liu <lywander@gmail.com>
2022-05-17 17:52:34 +0800

    eggs manager

--- Assets/Scripts/EggsManager.cs
+++ Assets/Scripts/EggsManager.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class EggsManager : MonoBehaviour
 {
     public Transform[] Eggs;
+    public float DroppedY = -10.0f;
+    public GameObject[] Enables;
 
-    // Start is called before the first frame update
+    private List<Transform> eggsList;
+
     void Start()
     {
-        
+        eggsList = new List<Transform>(Eggs);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < eggsList.Count; ++i)
+        {
+            Transform egg = eggsList[i];
+            if (egg.position.y < DroppedY)
+            {
+                eggsList.RemoveAt(i);
+                --i;
+                Destroy(egg.gameObject);
+            }
+        }
+
+        if (eggsList.Count == 0 && Enables != null)
+        {
+            foreach (var go in Enables)
+            {
+                go.SetActive(true);
+            }
+
+            Destroy(gameObject);
+        }
     }
 }
